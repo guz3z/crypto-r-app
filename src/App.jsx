095d5afc1efbfe3a3130e779aa98ed1673d93840
“yourGithubUsername” <your@emailadd.com>
2021-11-08 15:33:02 +0000

    add crypto API async await axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './style.css';
 
 function App() {
+    
+    const [ coins, setCoins ] = useState([]);
+
+    useEffect(() => {
+        async function fetchCryptoAPI() {
+            try {
+                let options = { headers: {'Accept': 'application/json'} }
+                let { data } = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false', options)
+                setCoins(data)
+            } catch (err) {
+                console.warn(err);
+                setCoins("Cant fetch coins API")
+            }
+        }
+        fetchCryptoAPI();
+    })
+
     return (
         <>
             <h1>ZeeCrypto Tracker</h1>
