1b7d9c63219331c6cba325bbff8aee46eef7a54b
“yourGithubUsername” <your@emailadd.com>
2021-11-08 16:29:04 +0000

    add Coins component

---
+++ src/components/Coins/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Coins = ({image, coinname, symbol, price, volume}) => {
+    return (
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto" />
+                    <h1>{coinname}</h1>
+                    <p className="coin-symbol">{symbol}</p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">${price}</p>
+                    <p className="coin-volume">${volume.toLocaleString()}</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Coins
