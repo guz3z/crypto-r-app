f9b559b9e46ac8f8df47a8ea47d91d8654c78eef
“yourGithubUsername” <your@emailadd.com>
2021-11-08 15:20:28 +0000

    install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './style.css';
 
 function App() {
     return (
         <>
- 
+            <h1>ZeeCrypto Tracker</h1>
+
+            <footer>Zerhjosh Guzman, 2021</footer>
         </>
     )
 };
