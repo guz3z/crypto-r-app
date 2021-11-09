454060ca483ce9211f46604cdc7db326e94395ef
“yourGithubUsername” <your@emailadd.com>
2021-11-09 00:04:08 +0000

    update coinheader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { SearchBar } from './components/SearchBar/index.jsx';
-import CoinHeader from './layout/CoinHeader/index.jsx';
 import './style.css';
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
     return (
         <>
             <h1>ZeeCrypto Tracker</h1>
-            <CoinHeader />
+            
             <SearchBar />
             
 
--- src/layout/CoinHeader/index.jsx
+++ src/layout/CoinHeader/index.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const CoinHeader = () => {
     return (
-        <div>
+        <div className="coin-header">
+        
+            <p className="coin-title">Coin</p>
+            <p className="ticker">Ticker</p>
+            <p className="title-price">Price</p>
+            <p className="title-change">Change (24h)</p>
+            <p className="title-volume">Volume (24h)</p>
+            <p className="title-marketcap">Mkt cap</p>
+      
             
         </div>
     )
