dd7e8379695434ef9cbfca2d82b913467bd0c81e
“yourGithubUsername” <your@emailadd.com>
2021-11-09 00:49:04 +0000

    add header

---
+++ src/layout/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header-cont">
+            <h1>ZeeCrypto Tacker</h1>
+            <div className="navbar">
+                <p>Coins</p>
+                <p>My Portfolio</p>
+                <p>News</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
